bb6a7b21a4fd4161127914b60e649c36e5e8506c
Lucas Fan <xfan0126@gmail.com>
2023-09-03 17:38:14 -0700

    finished resource page

---
+++ src/components/ResourcesPage/IntroductionSection.tsx
@@ -0,0 +1,19 @@
+import "./IntroductionSection.scss";
+
+function IntroductionSection() {
+    return (
+        <div className="introduction-section">
+            <div className="content">
+                <h2>Resources</h2>
+                <p>
+                    Here are some valuable resources that have been picked by
+                    the competition managers. They will be very helpful for
+                    studying not only for this competition, but competitive
+                    biology in general.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default IntroductionSection;
--- src/components/ResourcesPage/index.tsx
+++ src/components/ResourcesPage/index.tsx
@@ -1,5 +1,11 @@
+import IntroductionSection from "./IntroductionSection";
+
 function ResourcesPage() {
-    return <div className="resources-page"></div>;
+    return (
+        <div className="resources-page" style={{ height: "100vh" }}>
+            <IntroductionSection />
+        </div>
+    );
 }
 
 export default ResourcesPage;
